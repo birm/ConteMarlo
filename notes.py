885a624af5394e3c9f8f06c41c41eca57dd02337
Birm <birm@rbirm.us>
2016-09-18 00:23:21 -0400

    Code cleanup to match new generator pattern.

--- notes.py
+++ notes.py
@@ -11,11 +11,12 @@ Currently only works in 1d, but I want to generalize once I Get 1d working well.
 I also want to construct tests first, so I know I don't break things.
 
 Classes:
-    Resolver - Gets the values for the resolution set.
+    Resolver - A generator for the next distribution value pair
     Distribution - A distribution, samplable at [0,1]
 """
 class Resolver(Object):
     def __init__(self, distribution, *args, **kwargs):
+        """Parse through resolver inputs, and prepare for generator."""
         # check if is existing Distribution, else, make it once
         if isinstance(distribution, Distribution):
             self.distribution = distribution
@@ -23,35 +24,30 @@ class Resolver(Object):
             #  give it a "passed in" function
             self.distribution = Distribution(distribution)
         else:
-            raise BaseError("What?") # TODO fix
+            raise TypeError("Distribution passed is not callable.")
         self.layer = 1
         # set to one less than addressable int ln_2(max(INT) ) on system.
         if kwargs["high"]:
             self.high = kwargs["high"]
         else:
             self.high = (struct.calcsize("P") * 8)-1)
-
-
-    def eval_layer(self, n):
-        """Evaluate the distribution at the odd fractions of the power of 2."""
-        return [distribution(point)
-                for point in range(1,2**n) where m%2==1]
-        # not sure if this is more efficient than computing layer separately
+        # Finish preparation for generator for giving it an internal position.
+        self.position = 1
 
     def next(self, n):
-        self.out_vec.extend(self.eval_layer(self.layer))
-        self.layer = self.layer+1
+        """A generator to return the next [x,f(x)] pair from distribution."""
+        a = 0.0
+        while (a <= self.max):
+            a = 2.0 ** math.ceil((math.log((self.position + 1), 2)))
+            b = (2.0 * (a - self.position - 1) + 1) / a
+            yield [a, self.distribution(a)]
+            self.postion = self.position+1
 
-    def result(self, n):
-        # quickly order for linspace
-        # determine what fraction a position represents, make sure long
-        a = 2.0**math.ceil((math.log((n+1),2)))
-        return (2.0*(a-n-1)+1)/a
 
 class Distribution(Object):
     def __init__(name, *args, **kwargs):
-        """Parse through and return a distributon object to call later."""
-        """Any distribution input space should be [0,1]. Scale after."""
+        """Parse through and return a distributon object to call later.
+        Any distribution input space should be [0,1]. Scale after."""
         supported = ["normal", "custom"]
         self.args=args
         self.kwargs=kwargs
@@ -65,7 +61,7 @@ class Distribution(Object):
         else:
             self.name = "custom"
         # call the proper construction function
-        getattr(Distribution, self.name)())
+        getattr(Distribution, self.name)()
 
     def passed_in(self):
         """We already have the function, just go on."""
@@ -84,4 +80,5 @@ class Distribution(Object):
         else if callable(self.kwargs["fcn"]):
             self.fcn = self.kwargs["fcn"]
         else:
-            raise BaseError("What?") # TODO fix
+            raise ValueError("Did not find distribtion type " +
+                             self.kwargs["distribution"]) # TODO fix
